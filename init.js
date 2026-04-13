b0666b0ce4f3a77dd947d5ecf8a3246df7a00486
MahirUddin07 <mahudd070606@student.jenseneducation.se>
2026-04-13 08:53:39 +0200

    GG

--- init.js
+++ init.js
@@ -1,29 +1,34 @@
-let header= document.getElementsByTagName("header")[0].innerHTML = `
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+const header = document.getElementsByTagName("header")[0];
+header.innerHTML = `
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm sticky-top">
+  <div class="container">
+    <a class="navbar-brand fw-bold" href="#">
+      <i class="bi bi-shop"></i> Salong Nilsson
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="home.html">Hem</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="Omoss.html">Om oss</a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
             Produkter
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Hår Kräm</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Hårvård</a></li>
+            <li><a class="dropdown-item" href="#">Hudvård</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Alla produkter</a></li>
           </ul>
         </li>
       </ul>
+      <a href="kontakt.html" class="btn btn-outline-primary ms-lg-3">Kontakt</a>
     </div>
   </div>
-</nav>`
+</nav>`;
